92fc9fa8d05fed98b3c783f29c24203491b72ca5
PetrB <petr.budik96@gmail.com>
2018-12-28 22:20:08 +0100

    Initial Commit

---
+++ IOSSemestralWork/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  IOSSemestralWork
+//
+//  Created by Petr Budík on 28/12/2018.
+//  Copyright © 2018 Petr Budík. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
