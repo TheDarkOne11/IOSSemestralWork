237481e402a14b091278b0665d4f5d9e3e588be2
PetrB <petr@Petr-MacBook-Pro.local>
2018-12-29 19:23:35 +0100

    Added new models. Prepared navbar tableview.

---
+++ IOSSemestralWork/Model/MyRSSFeed.swift
@@ -0,0 +1,14 @@
+//
+//  MyRSSFeed.swift
+//  IOSSemestralWork
+//
+//  Created by Petr Budík on 29/12/2018.
+//  Copyright © 2018 Petr Budík. All rights reserved.
+//
+
+import Foundation
+
+class MyRSSFeed {
+    var title: String = ""
+    var link: String = ""
+}
