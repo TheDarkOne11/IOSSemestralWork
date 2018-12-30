e06801351edc16cdfb75e7998369a08e7f060cf6
PetrB <petr@Petr-MacBook-Pro.local>
2018-12-30 11:12:20 +0100

    NewFeedView design done.

--- IOSSemestralWork/Controller/NewFeedViewController.swift
+++ IOSSemestralWork/Controller/NewFeedViewController.swift
@@ -8,12 +8,27 @@
 
 import UIKit
 
-class NewFeedViewController: UIViewController {
-
+class NewFeedViewController: UITableViewController {
+    @IBOutlet weak var picker: UIPickerView!
+    @IBOutlet weak var pickerTableViewCell: UITableViewCell!
+    @IBOutlet weak var folderNameLabel: UILabel!
+    
+    var folders = [Folder]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
+        picker.delegate = self
+        picker.dataSource = self
+        
+        for i in 1...10 {
+            folders.append(Folder(with: "Folder\(i)"))
+        }
+        
+        folderNameLabel.text = folders.first?.title
     }
+    
+    // MARK: NavBar items
 
     @IBAction func cancelBtnPressed(_ sender: UIBarButtonItem) {
         dismiss(animated: true, completion: nil)
@@ -23,5 +38,48 @@ class NewFeedViewController: UIViewController {
         // TODO: Save feed, fetch its items
         
         dismiss(animated: true, completion: nil)
+    }
+    
+    // MARK: TableView methods
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        // TODO: Create expandable tableView instead of this?
+        
+        if indexPath.row == 0 && indexPath.section == 1 {
+            // Show/ Hide picker view
+            pickerTableViewCell.isHidden = !pickerTableViewCell.isHidden
+            folderNameLabel.textColor = pickerTableViewCell.isHidden == true ? UIColor.black : UIColor.red
+        }
+        
+        tableView.deselectRow(at: indexPath, animated: true)
+    }
+    
+}
+
+// MARK: UIPickerView methods
+
+extension NewFeedViewController: UIPickerViewDelegate, UIPickerViewDataSource {
+    /**
+     Number of columns.
+     */
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    
+    /**
+     Number of rows.
+     */
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return folders.count
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return folders[row].title
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        let selectedFolder = folders[row]
+        
+        folderNameLabel.text = selectedFolder.title
     }
 }
