bec09886dbad940d0a24d679fa40fd921b7abca3
PetrB <petr.budik96@gmail.com>
2019-07-01 13:25:35 +0200

    Added old files that will be transformed in the future.

---
+++ View/ItemCell/ItemCell.swift
@@ -0,0 +1,56 @@
+//
+//  ItemCell.swift
+//  IOSSemestralWork
+//
+//  Created by Petr Budík on 01/02/2019.
+//  Copyright © 2019 Petr Budík. All rights reserved.
+//
+
+import UIKit
+
+class ItemCell: UITableViewCell {
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var numOfItemsLabel: UILabel!
+    @IBOutlet weak var typeImage: UIImageView!
+    @IBOutlet weak var errorImage: UIImageView!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+    func setData(using folder: Folder) {
+        var count = 0
+        for feed in folder.myRssFeeds {
+            count += feed.unreadItemsCount()
+        }
+        
+        setData(title: folder.title, imgName: "folder", itemCount: count, true)
+    }
+    
+    func setData(using feed: MyRSSFeed) {
+        setData(title: feed.title, imgName: nil, itemCount: feed.unreadItemsCount(), feed.isOk)
+    }
+    
+    func setData(title: String?, imgName: String?, itemCount count: Int, _ errorHidden: Bool? = true) {
+        titleLabel.text = title
+        
+        if let imgName = imgName {
+            typeImage.image = UIImage(named: imgName)
+            typeImage.isHidden = false
+        } else {
+            typeImage.isHidden = true
+        }
+        
+        // Set errorImage/numOfItemsLabel
+        errorImage.isHidden = errorHidden!
+        numOfItemsLabel.isHidden = !errorImage.isHidden
+        numOfItemsLabel.text = "\(count)"
+    }
+    
+}
