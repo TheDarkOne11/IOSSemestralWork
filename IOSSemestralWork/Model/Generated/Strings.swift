b330b8b033766c9484723a6ef937a47f2f152a04
PetrB <petr.budik96@gmail.com>
2019-07-05 14:54:21 +0200

    Fixed Swiftgen script.

--- IOSSemestralWork/Model/Generated/Strings.swift
+++ IOSSemestralWork/Model/Generated/Strings.swift
@@ -11,8 +11,8 @@ import Foundation
 // swiftlint:disable explicit_type_interface function_parameter_count identifier_name line_length
 // swiftlint:disable nesting type_body_length type_name
 internal enum L10n {
-  /// Help
-  internal static let keyHelp = L10n.tr("Localizable", "key_help")
+  /// None
+  internal static let rootFolder = L10n.tr("Localizable", "rootFolder")
 
   internal enum Start {
     /// App version
