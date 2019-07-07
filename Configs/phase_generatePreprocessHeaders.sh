852285d3ac5269357a9455f7f3d620aac953a6d3
PetrB <petr.budik96@gmail.com>
2019-07-07 15:26:20 +0200

    Generate preprocessor file build phase repaired.

--- Configs/phase_generatePreprocessHeaders.sh
+++ Configs/phase_generatePreprocessHeaders.sh
@@ -4,5 +4,3 @@ HEADER_FILE="$PROJECT_DIR/info_preprocess_header.h"
 echo "// Auto-generated" > $HEADER_FILE
 echo "" >> $HEADER_FILE
 echo "#define BUILD_NUMBER $COMMITS" >> $HEADER_FILE
-
-touch $INFOPLIST_FILE
